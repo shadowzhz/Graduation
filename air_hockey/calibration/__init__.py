f7bb86e6d5ec16b9a3f27b0cafa15dedb19318c6
shadowzhz <2630646703@qq.com>
2026-08-31 18:31:11 +0800

    feat: export undistortion utility

--- air_hockey/calibration/__init__.py
+++ air_hockey/calibration/__init__.py
@@ -1,5 +1,6 @@
 """相机标定工具。"""
 
 from .camera_calibrator import CameraCalibrator, CalibrationResult
+from .undistort import Undistorter
 
-__all__ = ["CameraCalibrator", "CalibrationResult"]
+__all__ = ["CameraCalibrator", "CalibrationResult", "Undistorter"]
